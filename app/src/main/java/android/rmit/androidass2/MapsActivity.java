96b3e1ceca2f4d9a2284cd486c6237a698892f46
phamnguyenthoainhi <s3695349@rmit.edu.vn>
2019-11-12 20:25:02 +0700

    Move camera to location

--- app/src/main/java/android/rmit/androidass2/MapsActivity.java
+++ app/src/main/java/android/rmit/androidass2/MapsActivity.java
@@ -1,19 +1,29 @@
 package android.rmit.androidass2;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.IntentSender;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -21,7 +31,9 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -34,8 +46,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected FusedLocationProviderClient client;
     int MY_PERMISSIONS_REQUEST_LOCATION;
     ArrayList<MarkerData> markerData = new ArrayList<>();
+    protected static final int REQUEST_CHECK_SETTINGS = 0x1;
 
-
+    FusedLocationProviderClient fusedLocationProviderClient;
     Address address;
 
 
@@ -49,11 +62,66 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
         requestPermission();
         client = LocationServices.getFusedLocationProviderClient(MapsActivity.this);
+//        client.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+//            @Override
+//            public void onSuccess(Location location) {
+//                Toast.makeText(MapsActivity.this, location.getLatitude()+ " "+ location.getLongitude(), Toast.LENGTH_SHORT).show();
+//                LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
+//                MarkerData data = new MarkerData("702 Nguyễn Văn Linh, Tân Hưng, Quận 7, Hồ Chí Minh 700000, Việt Nam");
+//                MarkerData data2 = new MarkerData("Trường Sơn, Phường 2, Tân Bình, Hồ Chí Minh, Việt Nam");
+//                markerData.add(data);
+//                markerData.add(data2);
+//
+//                mMap.addMarker(new MarkerOptions().position(myLocation).title("HCM CIty"));
+//                createMarker(geoLocate(data).getLatitude(), geoLocate(data).getLongitude(), "heelo");
+//                createMarker(geoLocate(data2).getLatitude(), geoLocate(data2).getLongitude(), "goodbyeee");
+//                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(myLocation, 10);
+//                mMap.animateCamera(cameraUpdate);
+//            }
+//        });
 
 
 
 
+    }
 
+    protected void createLocationRequest() {
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                // All location settings are satisfied. The client can initialize
+                // location requests here.
+                // ...
+            }
+        });
+
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    // Location settings are not satisfied, but this can be fixed
+                    // by showing the user a dialog.
+                    try {
+                        // Show the dialog by calling startResolutionForResult(),
+                        // and check the result in onActivityResult().
+                        ResolvableApiException resolvable = (ResolvableApiException) e;
+                        resolvable.startResolutionForResult(MapsActivity.this,
+                                REQUEST_CHECK_SETTINGS);
+                    } catch (IntentSender.SendIntentException sendEx) {
+                        // Ignore the error.
+                    }
+                }
+            }
+        });
     }
 
     private Address geoLocate(MarkerData markerData) {
@@ -62,7 +130,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             List<Address> addressList = geocoder.getFromLocationName(markerData.getAddress(),1 );
             if(addressList.size() > 0){
                 address = addressList.get(0);
-                System.out.println("TEsting "+ address.getLongitude()+" ------ " + address.getLatitude());
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -90,22 +157,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
     }
 
-
     @SuppressLint("MissingPermission")
     public void getPosition(View view) {
         client.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
+
                 Toast.makeText(MapsActivity.this, location.getLatitude()+ " "+ location.getLongitude(), Toast.LENGTH_SHORT).show();
                 LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
                 MarkerData data = new MarkerData("702 Nguyễn Văn Linh, Tân Hưng, Quận 7, Hồ Chí Minh 700000, Việt Nam");
+                data.setTitle("data1");
                 MarkerData data2 = new MarkerData("Trường Sơn, Phường 2, Tân Bình, Hồ Chí Minh, Việt Nam");
+                data2.setTitle("data2");
                 markerData.add(data);
                 markerData.add(data2);
                 mMap.addMarker(new MarkerOptions().position(myLocation).title("HCM CIty"));
-                createMarker(geoLocate(data).getLatitude(), geoLocate(data).getLongitude(), "heelo");
-                createMarker(geoLocate(data2).getLatitude(), geoLocate(data2).getLongitude(), "goodbyeee");
 
+                for (int i = 0; i < markerData.size() ; i++) {
+                    createMarker(geoLocate(markerData.get(i)).getLatitude(), geoLocate(markerData.get(i)).getLongitude(), markerData.get(i).getTitle());
+                }
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(myLocation, 10);
+                mMap.animateCamera(cameraUpdate);
             }
         });
     }
